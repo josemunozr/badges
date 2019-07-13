e3fa7dcc8fb31361b9dfbee7a599bc91e2f3181f
José Muñoz R <jozeepp@gmail.com>
2019-07-13 19:24:30 -0400

    add styles

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -1,26 +1,28 @@
 import React from 'react';
+
+import './styles/Badge.css';
 import logoConf from '../images/badge-header.svg';
 
 class Badge extends React.Component {
   render() {
     return (
-      <div>
-        <div>
+      <div className="Badge">
+        <div className="Badge__header">
           <img src={logoConf} alt="logo de conferencia" />
         </div>
 
-        <div>
-          <img src="https://www.gravatar.com/avatar?d=identicon" alt="Avatar" />
+        <div className="Badge__section-name">
+          <img className="Badge__avatar" src="https://secure.gravatar.com/avatar/9080c6b09c3fe18281d069e6f17b82b3" alt="Avatar" />
           <h1>
             José <br /> Muñoz
           </h1>
         </div>
 
-        <div>
-          <p>Frontend Engineer</p>
-          <p>@jmunozr_</p>
+        <div className="Badge__section-info">
+          <h3>Frontend Engineer</h3>
+          <div>@jmunozr_</div>
         </div>
-        <div>#platziconf</div>
+        <div className="Badge__footer">#platziconf</div>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Badges from './components/Badge'
+import 'bootstrap/dist/css/bootstrap.css';
+
+import './global.css';
+import Badges from './components/Badge';
 
 ReactDOM.render(<Badges />, document.getElementById('root'));
