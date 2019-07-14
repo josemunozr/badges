d650fcd44b099a95cd7d2429539e96874b864124
José Muñoz R <jozeepp@gmail.com>
2019-07-14 19:00:24 -0400

    add page BadgeNew

--- src/index.js
+++ src/index.js
@@ -3,15 +3,6 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 
 import './global.css';
-import Badges from './components/Badge';
+import BadgeNew from './pages/BadgeNew';
 
-ReactDOM.render(
-  <Badges
-    firstName="José"
-    lastName="Muñoz"
-    titleJob="Frontend Engineer"
-    twitter="jmunozr_"
-    avatarUrl="https://secure.gravatar.com/avatar/9080c6b09c3fe18281d069e6f17b82b3"
-  />,
-  document.getElementById('root')
-);
+ReactDOM.render(<BadgeNew />, document.getElementById('root'));
---
+++ src/pages/BadgeNew.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import Badge from '../components/Badge';
+
+import './styles/BadgeNew.css';
+import header from '../images/badge-header.svg';
+import Navbar from '../components/Navbar';
+
+class BadgeNew extends React.Component {
+  render() {
+    return (
+      <div>
+        <Navbar />
+        <div className="BadgeNew__hero">
+          <img className="img-fluid" src={header} alt="Logo" />
+        </div>
+        <div className="container">
+          <div className="row">
+            <div className="col">
+              <Badge
+                firstName="José"
+                lastName="Muñoz"
+                twitter="jmunozr_"
+                avatarUrl="https://s.gravatar.com/avatar/9080c6b09c3fe18281d069e6f17b82b3?s=80"
+                titleJob="Frontend Engineer"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default BadgeNew;
