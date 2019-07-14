7d4bfc873f62fe5424727334cb3425973f597269
José Muñoz R <jozeepp@gmail.com>
2019-07-14 19:18:20 -0400

    initial Form in page BadgeNew

--- src/pages/BadgeNew.js
+++ src/pages/BadgeNew.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Badge from '../components/Badge';
+import BadgeForm from '../components/BadgeForm';
 
 import './styles/BadgeNew.css';
 import header from '../images/badge-header.svg';
@@ -15,7 +16,7 @@ class BadgeNew extends React.Component {
         </div>
         <div className="container">
           <div className="row">
-            <div className="col">
+            <div className="col-6">
               <Badge
                 firstName="José"
                 lastName="Muñoz"
@@ -23,6 +24,9 @@ class BadgeNew extends React.Component {
                 avatarUrl="https://s.gravatar.com/avatar/9080c6b09c3fe18281d069e6f17b82b3?s=80"
                 titleJob="Frontend Engineer"
               />
+            </div>
+            <div className="col-6">
+              <BadgeForm />
             </div>
           </div>
         </div>
