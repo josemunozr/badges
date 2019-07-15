f9d8a01e1399376205e0e712c428b7008f17223c
José Muñoz R <jozeepp@gmail.com>
2019-07-14 23:49:59 -0400

    state in BadgeNew shared with BadgeForm and Badge

--- src/components/BadgeForm.js
+++ src/components/BadgeForm.js
@@ -3,11 +3,11 @@ import React from 'react';
 class BadgeForm extends React.Component {
   state = {};
 
-  handleChange = e => {
-    this.setState({
-      [e.target.name]: e.target.value
-    });
-  };
+  // handleChange = e => {
+  //   this.setState({
+  //     [e.target.name]: e.target.value
+  //   });
+  // };
 
   handleClick = e => {
     console.log('button was clicked');
@@ -25,23 +25,23 @@ class BadgeForm extends React.Component {
         <form onSubmit={this.handleSubmit}>
           <div className="form-group">
             <label>First Name</label>
-            <input onChange={this.handleChange} type="text" className="form-control" name="firstName" value={this.state.firstName} />
+            <input onChange={this.props.onChange} type="text" className="form-control" name="firstName" value={this.state.firstName} />
           </div>
           <div className="form-group">
             <label>Last Name</label>
-            <input onChange={this.handleChange} type="text" className="form-control" name="lastName" value={this.state.lastName} />
+            <input onChange={this.props.onChange} type="text" className="form-control" name="lastName" value={this.state.lastName} />
           </div>
           <div className="form-group">
             <label>Email</label>
-            <input onChange={this.handleChange} type="email" className="form-control" name="email" value={this.state.email} />
+            <input onChange={this.props.onChange} type="email" className="form-control" name="email" value={this.state.email} />
           </div>
           <div className="form-group">
             <label>Job Title</label>
-            <input onChange={this.handleChange} type="text" className="form-control" name="jobTitle" value={this.state.jobTitle} />
+            <input onChange={this.props.onChange} type="text" className="form-control" name="jobTitle" value={this.state.jobTitle} />
           </div>
           <div className="form-group">
             <label>Twitter</label>
-            <input onChange={this.handleChange} type="text" className="form-control" name="twitter" value={this.state.twitter} />
+            <input onChange={this.props.onChange} type="text" className="form-control" name="twitter" value={this.state.twitter} />
           </div>
 
           <button onClick={this.handleClick} className="btn btn-primary">
--- src/pages/BadgeNew.js
+++ src/pages/BadgeNew.js
@@ -7,6 +7,19 @@ import header from '../images/badge-header.svg';
 import Navbar from '../components/Navbar';
 
 class BadgeNew extends React.Component {
+  state = {
+    form: {}
+  };
+
+  handleChange = e => {
+    this.setState({
+      form: {
+        ...this.state.form,
+        [e.target.name]: e.target.value
+      }
+    });
+  };
+
   render() {
     return (
       <div>
@@ -17,16 +30,17 @@ class BadgeNew extends React.Component {
         <div className="container">
           <div className="row">
             <div className="col-6">
-              <Badge
+              <Badge {...this.state.form} avatarUrl="https://s.gravatar.com/avatar/9080c6b09c3fe18281d069e6f17b82b3?s=80" />
+              {/* <Badge
                 firstName="José"
                 lastName="Muñoz"
                 twitter="jmunozr_"
                 avatarUrl="https://s.gravatar.com/avatar/9080c6b09c3fe18281d069e6f17b82b3?s=80"
                 titleJob="Frontend Engineer"
-              />
+              /> */}
             </div>
             <div className="col-6">
-              <BadgeForm />
+              <BadgeForm onChange={this.handleChange} />
             </div>
           </div>
         </div>
