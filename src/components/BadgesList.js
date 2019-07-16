df8f59893d864bfe7691ac8ab4094fa9b1b9ed97
José Muñoz R <jozeepp@gmail.com>
2019-07-16 00:04:28 -0400

    style to badge page

--- src/components/BadgesList.js
+++ src/components/BadgesList.js
@@ -1,15 +1,24 @@
 import React from 'react';
 
+import './styles/BadgesList.css';
+
 class BadgesList extends React.Component {
   render() {
     return (
-      <ul className="list-unstyled">
+      <ul className="list-unstyled BadgesList">
         {this.props.badges.map(badge => {
           return (
-            <li key={badge.id}>
-              <p>
-                {badge.firstName} {badge.lastName}
-              </p>
+            <li key={badge.id} className="Badge">
+              <img className="Badge__avatar" src={badge.avatarUrl} alt="avatar" />
+              <div className="Badge__info">
+                <div>
+                  {badge.firstName} {badge.lastName}
+                </div>
+                <div>
+                  <a href="twitter.com">@{badge.twitter}</a>
+                </div>
+                <p>{badge.jobTitle}</p>
+              </div>
             </li>
           );
         })}
