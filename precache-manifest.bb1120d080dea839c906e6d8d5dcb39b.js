107d855eb7afb6b196ed64b4a3d90a9d78d4ec1e
José Muñoz R <jozeepp@gmail.com>
2019-07-28 17:07:49 -0400

    Updates

---
+++ precache-manifest.bb1120d080dea839c906e6d8d5dcb39b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15bc21745971c2410750b5bb397e06f7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c302695467c7ece30a00",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "a182785208ca7554abdf",
+    "url": "/static/css/main.bf4c7fb4.chunk.css"
+  },
+  {
+    "revision": "c302695467c7ece30a00",
+    "url": "/static/js/2.e80335be.chunk.js"
+  },
+  {
+    "revision": "a182785208ca7554abdf",
+    "url": "/static/js/main.d8ade0f9.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "71addf454967249472471f5398055e3d",
+    "url": "/static/media/astronauts.71addf45.svg"
+  },
+  {
+    "revision": "973f5842f0c37eacb2eb491c62fc0032",
+    "url": "/static/media/badge-header.973f5842.svg"
+  },
+  {
+    "revision": "0026b862f1b05c3e27d471ba85c6ebc5",
+    "url": "/static/media/logo.0026b862.svg"
+  },
+  {
+    "revision": "f246f5f1fb702016606ee8c65e33c029",
+    "url": "/static/media/platziconf-logo.f246f5f1.svg"
+  },
+  {
+    "revision": "1d17360a685ac86023b90aa14fd2c62d",
+    "url": "/static/media/stars.1d17360a.svg"
+  }
+]);
